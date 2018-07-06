a6a49cd4fff6a1daa70476b3db3e733c198d0470
David Athobe <dathobek@gmail.com>
2018-07-06 17:45:54 +0100

    todoJS-Addeed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p> hello world</p>
+        <p> hello world hey</p>
        
       </div>
     );
