d227262ec15d5ba994bdfdc102c6407231e2c6a8
David Athobe <dathobek@gmail.com>
2018-07-06 18:05:50 +0100

    ref

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,11 @@ import React, {Component} from 'react'
 class TodoList extends Component {
     constructor(props){
         super(props);
+
+        this.state={
+          items : []
+        };
+
         this.addItem = this.addItem.bind(this)
     }
 
@@ -17,7 +22,8 @@ class TodoList extends Component {
             <div className="todoListMain">
               <div className="header">
                 <form onSubmit={this.addItem}>
-                  <input placeholder="enter task">
+                  <input ref={(a) => this._inputElement = a} 
+                       placeholder="enter task">
                   </input>
                   <button type="submit">add</button>
                 </form>
