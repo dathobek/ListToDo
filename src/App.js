1ce8863d12c35050d69cb3d238fdcece40899c47
David Athobe <dathobek@gmail.com>
2018-07-07 01:40:26 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import './index.css';
 import TodoList from './TodoList'
+import TodoItems from './TodoItems';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1> ATHOBE'S TODOLIST </h1>
         <TodoList/>
+        
        
       </div>
     );
